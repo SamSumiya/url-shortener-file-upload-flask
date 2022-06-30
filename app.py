6b6ea3970ed7980c25f74645103355aeb76e20bd
Sam Sumiya <samsan@Sams-Mac-Studio.local>
2022-06-30 09:16:24 -0700

    had some minor changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home(): 
+def home():
     return render_template('home.html')
 
 @app.route('/your-url', methods=['GET', 'POST'])
@@ -14,14 +14,16 @@ def your_url():
     if request.method == 'POST':
         urls = {}
 
-        if os.path.exists('urls.json'): 
-            with open('urls.json') as urls_file: 
-                urls = json.load(urls_file) 
-        if request.form['code'] in urls.keys(): 
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+
+        if request.form['code'] in urls.keys():
             return redirect(url_for('home'))
 
         urls[request.form['code']] = {'url': request.form['url']}
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
+
         return render_template('your_url.html', code=request.form['code'])
     return redirect(url_for('home'))
