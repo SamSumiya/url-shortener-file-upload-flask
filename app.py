d5e517aa54a8beb1a7d483666cbe997fb62e7183
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2022-06-30 20:24:06 -0700

    Dev (#2)
    * added a simple form
    
    * added lable tag in home.html
    
    * added lable for the second input in home.html
    
    * get user input through request and display it on your_url.html page
    
    * specified methods for the one route
    
    * used redirect to redirect users if they attempt to access the your-ulr via a post request
    
    * added json to dump and load json file
    
    * had some minor changes
    
    * added flash to show message and added environemnt variable to app.secret_key from zshenv file
    
    * add file works and it sends file to the right path
    
    * added a url redirect to how file info if it exists
    
    * added a redirect to take users to the page
    
    * added code to store image info to urls json file
    
    * shows error message if user does not have error message
    
    * modified the format
    
    * added an abort 404 default error
    
    * added a page not found page for 404 error
    
    * added session to display shortenered urls
    
    * added session to store information for displaying
    
    * used jsonify to display json format
    
    Co-authored-by: Sam Sumiya <samsan@Sams-Mac-Studio.local>

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import json
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
-
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ app.secret_key = env_var
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes=session.keys())
 
 
 @app.route('/your-url', methods=['GET', 'POST'])
@@ -34,32 +34,51 @@ def your_url():
         if 'url' in request.form.keys():
             urls[request.form['code']] = {'url': request.form['url']}
         elif 'file' in request.files.keys():
-            if 'file' not in request.files: 
+            if 'file' not in request.files:
                 flash('No file found...')
+
             file = request.files.get('file')
 
-            if file.filename == '':
+            if file.filename == '' or None:
                 flash('No selected file')
-                return redirect('home')
+                return redirect(url_for('home'))
 
             full_name = request.form['code'] + file.filename
 
-            if file and full_name: 
+            if file and full_name:
                 file_name = secure_filename(full_name)
-                file.save('/Users/samsan/Desktop/Coding/Flask Training/Flask/images/' + file_name)
+                file.save(
+                    '/Users/samsan/Desktop/Coding/Flask Training/Flask/static/user_files/' + file_name)
+                urls[request.form['code']] = {'file': full_name}
 
         with open('urls.json', 'w') as url_file:
-            json.dump(urls, url_file)
+            deserialized = json.dump(urls, url_file)
+            session[request.form['code']] = True
             flash('Your have successfully stord an url!!')
         return render_template('your_url.html', code=request.form['code'])
     return redirect(url_for('home'))
 
+
 @app.route('/<string:code>')
-def url_redirect(code): 
+def url_redirect(code):
     if os.path.exists('urls.json'):
+
         with open('urls.json') as json_file:
             deserialized = json.load(json_file)
+
             if code in deserialized.keys():
-                return deserialized[code]['url']
-            flash('Not found')
-            return redirect(url_for('home'))
+                if 'url' in deserialized[code].keys():
+                    return redirect(deserialized[code]['url'])
+                else:
+                    return redirect(url_for('static', filename='user_files/' + deserialized[code]['file']))
+    return abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
