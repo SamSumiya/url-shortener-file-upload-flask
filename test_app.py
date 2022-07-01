7c3a085ee5a45a0487f909d820ae0e69e055c792
Sam Sumiya <samsan@Sams-Mac-Studio.local>
2022-07-01 13:18:40 -0700

    added pytest to check if there is a button on the main page

---
+++ test_app.py
@@ -0,0 +1,7 @@
+import pytest
+from urlshort import create_app 
+
+
+def test_shorten(client):
+    response = client.get('/')
+    assert b'Shorten' in response.data
--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -6,7 +6,7 @@ import os
 def create_app(test_config=None):
 
     app = Flask(__name__)
-    
+
     env_var = os.environ.get('TEST_ENV_VAR')
     app.secret_key = env_var
 
